77ff54b73ba2c013c5432193de219b0c9a21636c
leejongil1046 <100816520+leejongil1046@users.noreply.github.com>
2023-12-03 04:24:58 +0900

    Feat: 영화 API를 활용한 index 페이지 만들기

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const path = require("path");
-
+const fetch = require("node-fetch");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public")); // 정적 파일 제공
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/api/movies", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=download_count"
+    );
+    const moviesData = await response.json();
+    res.json(moviesData);
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    res.status(500).send("Error fetching movies");
+  }
 });
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,34 @@
-// 검색 기능, 댓글 기능 등에 필요한 JavaScript 코드
-document.addEventListener("DOMContentLoaded", function () {
-  const searchButton = document.querySelector(".search-container button");
-  searchButton.addEventListener("click", function () {
-    const searchTerm = document.querySelector(".search-container input").value;
-    console.log(`검색어: ${searchTerm}`);
-    // 검색 기능 구현
-  });
+// DOM이 로드되었을 때 실행
+document.addEventListener("DOMContentLoaded", () => {
+  // '/api/movies' 경로로 HTTP GET 요청을 보냄
+  fetch("/api/movies")
+    .then((response) => {
+      // 응답을 JSON 형식으로 파싱
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // 파싱된 데이터 처리
+      const moviesContainer = document.querySelector("#movies-container");
+      // 로딩 문구 제거
+      const loadingMessage = moviesContainer.querySelector("#loading-message");
+      if (loadingMessage) {
+        loadingMessage.innerHTML = ""; // 'p' 태그의 내용을 비움
+      }
+      data.data.movies.forEach((movie) => {
+        // 각 영화에 대한 HTML 요소 생성 및 추가
+        const movieElement = document.createElement("div");
+        movieElement.innerHTML = `
+                <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                <h3>${movie.title}</h3>
+                `;
+        moviesContainer.appendChild(movieElement);
+      });
+    })
+    .catch((error) => {
+      // 오류 처리
+      console.error("Fetch error:", error);
+    });
 });

fc6d4f2d22629755ae9c4f72fa0714b737397c1f
leejongil1046 <100816520+leejongil1046@users.noreply.github.com>
2023-12-05 00:19:00 +0900

    Fix: 회원가입 창 가운데 정렬, 비밀번호 유효성 메세지 위치 조정

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,15 @@
 // DOM이 로드되었을 때 실행
 document.addEventListener("DOMContentLoaded", () => {
+  // 로딩 메시지 추가
+  const loadingMessage = document.createElement("p");
+  loadingMessage.textContent = "Loading...";
+  loadingMessage.id = "loading-message";
+  document.body.appendChild(loadingMessage);
+
+  fetchMoviesData();
+});
+
+function fetchMoviesData() {
   // '/api/movies' 경로로 HTTP GET 요청을 보냄
   fetch("/api/movies")
     .then((response) => {
@@ -10,25 +20,34 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     })
     .then((data) => {
+      // 로딩 메시지 제거
+      const loadingMessage = document.getElementById("loading-message");
+      if (loadingMessage) {
+        document.body.removeChild(loadingMessage);
+      }
       // 파싱된 데이터 처리
       const moviesContainer = document.querySelector("#movies-container");
       // 로딩 문구 제거
-      const loadingMessage = moviesContainer.querySelector("#loading-message");
-      if (loadingMessage) {
-        loadingMessage.innerHTML = ""; // 'p' 태그의 내용을 비움
-      }
       data.data.movies.forEach((movie) => {
         // 각 영화에 대한 HTML 요소 생성 및 추가
+
         const movieElement = document.createElement("div");
         movieElement.innerHTML = `
-                  <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                  <a href="/movie">
+                    <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                  </a>          
                   <h3>${movie.title}</h3>
                   `;
         moviesContainer.appendChild(movieElement);
+
+        movieElement.querySelector("a").addEventListener("click", (event) => {
+          event.preventDefault(); // 기본 이벤트 방지
+          window.location.href = `/movie/${movie.id}`; // 영화 상세 페이지로 이동
+        });
       });
     })
     .catch((error) => {
       // 오류 처리
       console.error("Fetch error:", error);
     });
-});
+}

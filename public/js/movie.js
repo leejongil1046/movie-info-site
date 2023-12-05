c20cb6ab50b41cd53ea0822142bc1df7bbe9b45a
leejongil1046 <100816520+leejongil1046@users.noreply.github.com>
2023-12-06 03:12:53 +0900

    Feat: 영화 상세 페이지에 배경이미지 추가, 오버레이로 밝기 조정

--- public/js/movie.js
+++ public/js/movie.js
@@ -24,6 +24,17 @@ function fetchMovieDetails(movieId) {
         document.body.removeChild(loadingMessage);
       }
       if (data.status === "ok") {
+        const backgroundLogin = document.querySelector(".login-box-movie");
+        if (backgroundLogin) {
+          backgroundLogin.style.backgroundColor = "rgba(255, 255, 255, 0)";
+        }
+        // API 응답에서 이미지 URL 추출
+        const imageUrl = data.data.movie.background_image;
+        // body 태그의 배경 이미지로 설정
+        document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('${imageUrl}')`;
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundPosition = "center center";
         document.querySelector("#movie-image").style.display = "block";
         document.querySelector(".movie-story").style.display = "block";
         document.querySelector(".movie-reviews").style.display = "block";
@@ -35,20 +46,20 @@ function fetchMovieDetails(movieId) {
         document.querySelector("#movie-genres").innerHTML =
           data.data.movie.genres.join(" / ");
         document.querySelector("#movie-runtime").innerHTML =
-          '<span id="runtime-label"><i class="fa-regular fa-clock"></i></span> ' +
+          '<span id="runtime-label"><i class="fa-regular fa-clock"></i></span> &nbsp;' +
           data.data.movie.runtime +
           " min";
         let language;
         if (data.data.movie.language === "en") language = "English";
         document.querySelector("#movie-language").innerHTML =
-          '<span id="language-label"><i class="fa-solid fa-volume-high"></i></span> ' +
+          '<span id="language-label"><i class="fa-solid fa-volume-high"></i></span> &nbsp;' +
           language;
         document.querySelector("#movie-rating").innerHTML =
-          '<span id="rating-label"><i class="fa-solid fa-star"></i></span> ' +
+          '<span id="rating-label"><i class="fa-solid fa-star"></i></span> &nbsp;' +
           data.data.movie.rating +
           "/10";
         document.querySelector("#movie-like").innerHTML =
-          '<span id="like-label"><i class="fa-solid fa-heart"></i></span> ' +
+          '<span id="like-label"><i class="fa-solid fa-heart"></i></span> &nbsp;' +
           data.data.movie.like_count;
         document.querySelector("#movie-description").innerHTML =
           '<h2 id="description-label">Description</h2> &nbsp;&nbsp;' +

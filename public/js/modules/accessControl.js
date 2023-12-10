c9d3da5b7299a35f7922f81f7b8e603410c1af08
leejongil1046 <100816520+leejongil1046@users.noreply.github.com>
2023-12-10 22:10:30 +0900

    Feat: movie.html에서 로그인 안 한 상태에서 접근 제한 구현

---
+++ public/js/modules/accessControl.js
@@ -0,0 +1,30 @@
+function restrictAccessToNotLoggedinUsers() {
+  const likeIcon = document.querySelector("#like-label i");
+  const ratingStar = document.querySelector(".rating");
+  const submitButton = document.querySelector("#review-submit-btn");
+  const reviewTextarea = document.querySelector("#review-text");
+  const emailInput = document.querySelector("#email");
+
+  likeIcon.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default action
+    alert("Please log in to like movies.");
+    emailInput.focus();
+  });
+
+  ratingStar.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default action
+    alert("Please log in to rate movies.");
+    emailInput.focus();
+  });
+
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default action
+    alert("Please log in to submit reviews.");
+    emailInput.focus();
+  });
+
+  reviewTextarea.disabled = true;
+  reviewTextarea.placeholder = "Please log in to write a review.";
+}
+
+export { restrictAccessToNotLoggedinUsers };
--- public/js/movie.js
+++ public/js/movie.js
@@ -23,6 +23,8 @@ import {
   displayTotalLikes,
 } from "/js/modules/movieLikesAPI.js";
 
+import { restrictAccessToNotLoggedinUsers } from "/js/modules/accessControl.js";
+
 // DOM이 로드되었을 때 실행
 document.addEventListener("DOMContentLoaded", async () => {
   // URL에서 ID를 추출
@@ -50,9 +52,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     reviewContainer(username);
     // 여기서 추가 로직을 수행할 수 있습니다.
   } else {
+    restrictAccessToNotLoggedinUsers();
     console.log("User is not logged in.");
     // 로그인하지 않은 상태에 대한 처리를 할 수 있습니다.
   }
-
   displayTotalLikes(movieId);
 });
